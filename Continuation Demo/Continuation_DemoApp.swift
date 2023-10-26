175ad615861e2c5029910c859e6d783095eb9b0a
David Rynn <davidrynn@gmail.com>
2023-10-26 16:26:29 -0400

    Initial Commit

---
+++ Continuation Demo/Continuation_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Continuation_DemoApp.swift
+//  Continuation Demo
+//
+//  Created by David Rynn on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Continuation_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
